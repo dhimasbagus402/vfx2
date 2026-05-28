69914708ecb6d59d33a2c56691f2cc38d5a60d77
dhimasbagus402 <dhimasbagus402@gmail.com>
2026-05-28 16:20:45 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "=== Pulling latest update ==="
-git -C ~/vfx pull
-chmod +x ~/vfx/update.sh
-echo "=== Running install.sh ==="
-bash ~/vfx/doinstall.sh
-echo "=== Update selesai! ==="
