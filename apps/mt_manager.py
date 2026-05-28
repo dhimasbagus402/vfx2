8c39214c11dbc52c0bb2209319da5f27e6c2be27
dhimasbagus402 <dhimasbagus402@gmail.com>
2026-05-28 16:43:48 +0700

    Update mt_manager.py

--- apps/mt_manager.py
+++ apps/mt_manager.py
@@ -534,7 +534,7 @@ class ProgressBar(tk.Canvas):
 class MTManager:
     def __init__(self, root):
         self.root = root
-        self.root.title("MetaTrader Manager 4")
+        self.root.title("MetaTrader Manager 3")
         self.root.geometry("1180x700")
         self.root.configure(bg=BG)
         self.root.resizable(True, True)
